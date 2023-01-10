fb45d84513708110056f3bb8e7a2f7a6fa63065c
IFrakas <s.franciskamau@gmail.com>
2023-01-10 06:09:48 -0800

    complete source code

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
