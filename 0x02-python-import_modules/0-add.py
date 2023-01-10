59ba72be7b3e864a5055b1ce30457864eb50c38b
IFrakas <s.franciskamau@gmail.com>
2023-01-09 22:11:20 -0800

    imports the function def add(a, b).

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ from add_0 import add
 
 a = 1
 b = 2
-if _name_ == "_main_":
-    print("{:d} + {:d}".format(a, b add(a, b)))
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b add(a, b)))
